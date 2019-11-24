9ed790fe4817874e1c5f9a430e936ab8ad7e1a50
justDredd <mrmagnusrus@gmail.com>
2019-11-24 23:32:32 +0300

    added socket_client

--- Socket_client.py
+++ Socket_client.py
@@ -2,12 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import pygame
 
-sock = socket.socket()
-sock.connect(('localhost', 9090))
-sock.send('hello, world!')
 
-data = sock.recv(1024)
-sock.close()
+print ("Приветствую Дредд, введи уровень: ")
+level:int = input ()
+print("Введи ник: ")
+name:str = input ()
 
-print (data)
+
+result: str = "magnus;magnus161;./csgoserver console;wcs_setlblvl " + level + " " + name
+print (result)
+print ("magnus\nmrmagnus161\n./csgoserver console\nwcs_setlblvl + " + level + " " + name)
+input('Press ENTER to exit')
+
+#sock = socket.socket()
+#sock.connect(('localhost', 9090))
+#sock.send('hello, world!')
+
+#data = sock.recv(1024)
+#sock.close()
+
+#print (data)
