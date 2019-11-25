362e05dcc6dfe33879cbf7e4179b557e2ac44425
justDredd <mrmagnusrus@gmail.com>
2019-11-25 20:00:46 +0300

    added some fuction

--- LocalManagement.py
+++ LocalManagement.py
@@ -2,25 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import socket
-import pygame
 
-
-print ("Приветствую Дредд, введи уровень: ")
-level:int = input ()
+print("Приветствую Дредд, введи сумму: ")
+sum = int(input())
 print("Введи ник: ")
-name:str = input ()
+name: str = input()
+print("Введи скидку: ")
+discountLevel = int(input())
 
+totalLvl = (sum * 6 / 100) * (100 + discountLevel)
+if 100 <= sum < 200:
+    totalLvl += 50
+if 200 <= sum < 500:
+    totalLvl += 100
+if 500 <= sum < 1000:
+    totalLvl += 450
+if sum >= 1000:
+    totalLvl += 800
 
-result: str = "magnus;magnus161;./csgoserver console;wcs_setlblvl " + level + " " + name
-print (result)
-print ("magnus\nmrmagnus161\n./csgoserver console\nwcs_setlblvl + " + level + " " + name)
+# result: str = "magnus;magnus161;./csgoserver console;wcs_setlblvl " + str(totalLvl) + " " + name
+# print (result)
+print("magnus\nmrmagnus161\n./csgoserver console\nwcs_setlblvl + " + str(totalLvl) + " " + name)
 input('Press ENTER to exit')
 
-#sock = socket.socket()
-#sock.connect(('localhost', 9090))
-#sock.send('hello, world!')
+# sock = socket.socket()
+# sock.connect(('localhost', 9090))
+# sock.send('hello, world!')
 
-#data = sock.recv(1024)
-#sock.close()
+# data = sock.recv(1024)
+# sock.close()
 
-#print (data)
+# print (data)
