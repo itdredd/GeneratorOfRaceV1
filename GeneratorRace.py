c2d36801a1e9c0089c93c19d19ea3181bc50b28e
justDredd <mrmagnusrus@gmail.com>
2019-11-28 21:36:53 +0300

    Changed structure of program

--- GeneratorRace.py
+++ GeneratorRace.py
@@ -8,4 +8,27 @@ def GeneratorSkills(maxSkills):
     z = 0
     while z <= maxSkills:
         temp = random.randrange(0, 10)
-        if temp == listSkills.index('speed') or temp == listSkills.index()
+        if temp == listSkills.index('speed'):
+            valueOfSkill = random.randrange(100, 150)
+            additionalValue = random.randrange(10, 40)
+            settingsSkill : str = '"soeed = "\n"', valueOfSkill, ';', valueOfSkill+additionalValue, ';', valueOfSkill+additionalValue*2, ';', valueOfSkill+additionalValue*3, '"'
+        if temp == listSkills.index('gravity')
+            valueOfSkill = random.randrange(60, 100)
+            additionalValue = random.randrange(5, 8)
+            settingsSkill : str = '"gravity ="\n', valueOfSkill, ';', valueOfSkill+additionalValue, ';', valueOfSkill+additionalValue*2, ';', valueOfSkill+additionalValue*3, '"'
+        if temp == listSkills.index('hp'):
+            valueOfSkill = random.randrange(100, 150)
+            additionalValue = random.randrange(10, 40)
+            settingsSkill: str = '"hp = "\n"', valueOfSkill, ';', valueOfSkill + additionalValue, ';', valueOfSkill + additionalValue * 2, ';', valueOfSkill + additionalValue * 3, '"'
+        if temp == listSkills.index(dmg):
+            valueOfSkill : float = random.randrange(100, 150)
+            valueOfSkill /= 100
+
+
+
+
+
+
+
+
+
