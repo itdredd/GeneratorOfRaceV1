15a92cd91bb9bd61b3fa34013fec6ffe27fd2131
justDredd <mrmagnusrus@gmail.com>
2019-12-02 20:22:48 +0300

    Finish version 1.0.1

--- GeneratorRace.py
+++ GeneratorRace.py
@@ -11,7 +11,7 @@ tempList = []
 class Race(object):
 
     def DefaultForm(self, tempList):
-        print('\"name\"\n{\n\t\"name\"            \"name\"\n\t\"required\"        \"' + str(required) +  '\"\n\t\"author\"          \"Magnus\"\n\t\"category\"        \"Девятый дивизион(30000-45000)\"\n\t\"maxlvl\"          \"1000\"\n\t\"teamlimit\"       \"0\"\n\t\"skillamount\"     \"' + str(maxSkills) + '\"\n\t\"skilllvls\"       \"0\"\n\t\"skillsets\"       \"4\"\n\t\"skillnames\"      \"' + str((maxSkills-1)*';') + '\"\n\t\"skilldesc\"       \"none\"')
+        print('\"name\"\n{\n\t\"name\"            \"name\"\n\t\"required\"        \"' + str(required) +  '\"\n\t\"author\"          \"Magnus\"\n\t\"category\"        \"Девятый дивизион(30000-45000)\"\n\t\"maxlvl\"          \"1000\"\n\t\"teamlimit\"       \"0\"\n\t\"skillamount\"     \"' + str(maxSkills) + '\"\n\t\"skilllvls\"       \"0\"\n\t\"skillsets\"       \"4\"\n\t\"skillnames\"      \"none\"\n\t\"skilldesc\"       \"none\"')
 
 
     def GeneratorSkills(self, maxSkills):
@@ -30,18 +30,19 @@ class Race(object):
                 round(additionalValue, 3)
                 round(valueOfSkill, 3)
                 print('\t\"skill' + str(int(z+1)) + '\"          \"speed =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(round(valueOfSkill,3)) + ';' + str(round(float(valueOfSkill+additionalValue), 3)) + ';' + str(round(float(valueOfSkill+additionalValue*2),3)) + ';' + str(round(float(valueOfSkill+additionalValue*3), 3)) + '\"')
+                tempList.append('speed')
             elif temp == listSkills.index('gravity'):
                 valueOfSkill = random.randrange(60, 100)
                 additionalValue = random.randrange(5, 8)
                 additionalValue /= 100
                 valueOfSkill /= 100
                 print('\t\"skill' + str(int(z+1)) + '\"          \"gravity =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill) + ';' + str(round(float(valueOfSkill-additionalValue), 3)) + ';' + str(round(float(valueOfSkill-additionalValue*2), 3)) + ';' + str(round(float(valueOfSkill-additionalValue*3), 3)) + '\"')
-
+                tempList.append('gravity')
             elif temp == listSkills.index('hp'):
                 valueOfSkill = random.randrange(100, 150)
                 additionalValue = random.randrange(10, 40)
                 print('\t\"skill' + str(int(z+1)) + '\"          \"hp =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill) + ';' + str(int(valueOfSkill + additionalValue)) + ';' + str(int(valueOfSkill + additionalValue * 2)) + ';' + str(int(valueOfSkill + additionalValue * 3)) + '\"')
-
+                tempList.append('hp')
             elif temp == listSkills.index('dmg'):
                 valueOfSkill = random.randrange(100, 150)
                 valueOfSkill /= 100
@@ -56,7 +57,7 @@ class Race(object):
                 elif skillChange == 0:
                     additionalValueChange = random.randrange(10, 15)
                 print('\t\"skill' + str(int(z+1)) + '\"          \"dmg =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill), str(skillChange) + ';' + str(int(valueOfSkill+additionalValue)), str(int(skillChange+additionalValueChange)) + ';' + str(round(valueOfSkill+additionalValue*2, 3)), str(int(skillChange+additionalValueChange*2)) + ';' + str(round(valueOfSkill+additionalValue*3, 3)), str(int(skillChange+additionalValueChange*3)) + '\"')
-
+                tempList.append('dmg')
             elif temp == listSkills.index('freeze'):
                 valueOfSkill= random.randrange(100, 150)
                 valueOfSkill /= 100
@@ -71,7 +72,7 @@ class Race(object):
                 elif skillChange == 0:
                     additionalValueChange = random.randrange(10, 15)
                 print('\t\"skill' + str(int(z+1)) + '\"          \"freeze =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill), str(skillChange) + ';' + str(round(valueOfSkill + additionalValue, 3)), str(int(skillChange + additionalValueChange)) + ';' + str(round(valueOfSkill + additionalValue * 2, 3)), str(int(skillChange + additionalValueChange * 2)) + ';' + str(round(valueOfSkill + additionalValue * 3, 3)), str(int(skillChange + additionalValueChange * 3)) + '\"')
-
+                tempList.append('freeze')
             elif temp == listSkills.index('burn'):
                 valueOfSkill= random.randrange(100, 150)
                 valueOfSkill /= 100
@@ -86,7 +87,7 @@ class Race(object):
                 elif skillChange == 0:
                     additionalValueChange = random.randrange(10, 15)
                 print('\t\"skill' + str(int(z+1)) + '\"          \"burn =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill), str(skillChange) + ';' + str(round(float(valueOfSkill + additionalValue), 3)) + ' ' + str(int(skillChange + additionalValueChange)) + ';' + str(round(float(valueOfSkill + additionalValue * 2), 3)), str(int(skillChange + additionalValueChange * 2)) + ';' + str(round(float(valueOfSkill + additionalValue * 3), 3)), str(int(skillChange + additionalValueChange * 3)) + '\"')
-
+                tempList.append('burn')
             elif temp == listSkills.index('vampire'):
                 valueOfSkill= random.randrange(100, 150)
                 valueOfSkill /= 100
@@ -101,24 +102,24 @@ class Race(object):
                 elif skillChange == 0:
                     additionalValueChange = random.randrange(10, 15)
                 print('\t\"skill' + str(int(z+1)) + '\"          \"vampire =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill), str(skillChange) + ';' + str(round(valueOfSkill+additionalValue, 3)), str(int(skillChange+additionalValueChange)) + ';' + str(round(valueOfSkill+additionalValue*2, 3)), str(int(skillChange+additionalValueChange*2)) + ';' + str(round(valueOfSkill+additionalValue*3, 3)), str(int(skillChange+additionalValueChange*3)) + '\"')
-
+                tempList.append('vampire')
             elif temp == listSkills.index('longjump'):
                 valueOfSkill = random.randrange(110, 600)
                 additionalValue = random.randrange(10, 60)
-                print('\t\"skill' + str(int(z+1)) + '\"          \"longjump =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill) + ';' + str(int(valueOfSkill + additionalValue)) + ';' + str(int(valueOfSkill + additionalValue * 2)) + ';' + str(int(valueOfSkill + additionalValue * 3)) + '\"')
-
+                print('\t\"skill' + str(int(z+1)) + '\"          \"longjump =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(float(valueOfSkill)) + ';' + str(float(valueOfSkill + additionalValue)) + ';' + str(float(valueOfSkill + additionalValue * 2)) + ';' + str(float(valueOfSkill + additionalValue * 3)) + '\"')
+                tempList.append('longjump')
             elif temp == listSkills.index('evasion'):
                 valueOfSkill = random.randrange(0, 60)
                 additionalValue = 0
                 if valueOfSkill < 50:
                     additionalValue = random.randrange(3, 7)
                 print('\t\"skill' + str(int(z+1)) + '\"          \"evasion\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill) + ';' + str(int(valueOfSkill + additionalValue)) + ';' + str(int(valueOfSkill + additionalValue * 2)) + ';' + str(int(valueOfSkill + additionalValue * 3)) + '\"')
-
+                tempList.append('evasion')
             elif temp == listSkills.index('step'):
                 valueOfSkill = random.randrange(100, 200)
                 additionalValue = random.randrange(5, 20)
                 print('\t\"skill' + str(int(z+1)) + '\"          \"step\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill) + ';' + str(int(valueOfSkill + additionalValue)) + ';' + str(int(valueOfSkill + additionalValue * 2)) + ';' + str(int(valueOfSkill + additionalValue * 3)) + '\"')
-
+                tempList.append('step')
             elif temp == listSkills.index('paralyze'):
                 valueOfSkill = random.randrange(100, 150)
                 valueOfSkill /= 100
@@ -133,19 +134,27 @@ class Race(object):
                 elif skillChange == 0:
                     additionalValueChange = random.randrange(10, 15)
                 print('\t\"skill' + str(int(z+1)) + '\"          \"paralyze =\"\n\t\"setting' + str(int(z+1)) + '\"', '       \"' + str(valueOfSkill), str(skillChange) + ';' + str(int(valueOfSkill + additionalValue)),  str(int(skillChange + additionalValueChange)) + ';' + str(round(valueOfSkill + additionalValue * 2, 3)), str(int(skillChange + additionalValueChange * 2)) + ';' + str(round(valueOfSkill + additionalValue * 3, 3)), str(int(skillChange + additionalValueChange * 3)) + '\"')
+                tempList.append('paralyze')
             else:
                 settingSkill = 'Error'
             #obj.Output(0, settingSkill, temp, valueOfSkill, skillChange, additionalValue, additionalValueChange)
             z+=1
         print('}')
-        print('Generate is over')
 
 
     def Output(self, settingSkill):
         print(settingSkill)
 
+    def Addditional(self):
+        for i in tempList:
+            print(i,end = ';')
+
+
+
+
 obj = Race
 obj.DefaultForm(0, tempList)
 obj.GeneratorSkills(0, maxSkills)
+obj.Addditional(0)
 input()
 
